fd2d118c6472f899dea34b7653bada8f999f910b
Ahmad Fuad <anwariahmadfawad2@gmail.com>
2023-12-26 22:12:55 +0430

    title changed

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Count-Down-App",
+  base: "/Countdownapp",
   plugins: [react()],
 })
